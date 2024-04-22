9c19b79eccabd805d49c88506430f8f37f2689e3
Divyanshu Dugar <divyu2004@gmail.com>
2024-04-22 19:22:44 -0400

    added dcl commands

--- dcl.sql
+++ dcl.sql
@@ -1,3 +1,11 @@
 /* 
 DCL - Data Control Language
 */
+
+REVOKE ALL PRIVILEGES ON newemployees FROM PUBLIC;
+
+GRANT SELECT ON newemployees TO dbs211_241zbb27;
+
+GRANT INSERT, UPDATE, DELETE ON newemployees TO dbs211_241zbb27;
+
+REVOKE ALL PRIVILEGES ON newemployees FROM dbs211_241zbb27;
